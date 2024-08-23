91e88f906e70212cfc4cca48f1b77f794a04bcfe
Anubhav-Raj <rajputanubhav65@gmail.com>
2024-08-23 16:42:33 +0530

    add  body parser

--- backend/index.js
+++ backend/index.js
@@ -29,8 +29,11 @@ if (cluster.isPrimary) {
   connectDB();
 
   app.use(cors());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  // app.use(express.json());
+  // app.use(express.urlencoded({ extended: true }));
+
+  app.use(bodyParser.json()); // for parsing application/json
+  app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
   // app.use(express.static(path.resolve(__dirname, "./dist")));
 
   // Routes
